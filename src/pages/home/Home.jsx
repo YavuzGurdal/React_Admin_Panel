152c94b249f3e95c5f7459c67d08952ad0a37c3c
Yavuz <yavuz@Yavuz-MacBook-Pro.local>
2022-07-10 12:11:09 +0200

    table

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,5 +1,6 @@
 import Chart from '../../components/chart/Chart'
 import Featured from '../../components/featured/Featured'
+import Table from '../../components/table/Table'
 import Navbar from '../../components/navbar/Navbar'
 import Sidebar from '../../components/sidebar/Sidebar'
 import Widget from '../../components/widget/Widget'
@@ -21,6 +22,10 @@ const Home = () => {
                 <div className="charts">
                     <Featured />
                     <Chart />
+                </div>
+                <div className="listContainer">
+                    <div className="listTitle">Latest Transactions</div>
+                    <Table />
                 </div>
             </div>
         </div>
